0b34d67d9b1fd5a51dd45ca94e875ef04eeeed84
molimolily <hiroto07300730@gmail.com>
2024-11-10 02:32:06 +0900

    セグメンテーション追加

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 
 # Initialize Face Mesh
 mp_face_mesh = mp.solutions.face_mesh
@@ -25,24 +26,39 @@ pose = mp_pose.Pose(
     min_tracking_confidence=0.5
 )
 
-# Initialize drawing
+# Initialize Selfie Segmentation
+mp_selfie_segmentation = mp.solutions.selfie_segmentation
+selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation(model_selection=1)
+
+# Initialize MediaPipe drawing
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
 cap = cv2.VideoCapture(0)
 
+# run
 while cap.isOpened():
     success, image = cap.read()
     if not success:
+        print("Ignoring empty camera frame.")
         continue
 
     # BGR to RGB
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # Process the image and find face mesh
+    # Process
     face_mesh_results = face_mesh.process(image_rgb)
     hand_results = hands.process(image_rgb)
     pose_results = pose.process(image_rgb)
+    segmentation_results = selfie_segmentation.process(image_rgb)
+
+    # gb
+    green_bg = np.zeros(image.shape, dtype=np.uint8)
+    green_bg[:] = (0, 255, 0)
+
+    # Apply the segmentation mask
+    condition = np.stack((segmentation_results.segmentation_mask,) * 3, axis=-1) > 0.1
+    image = np.where(condition, image, green_bg)
 
     # Draw Face Mesh landmarks
     if face_mesh_results.multi_face_landmarks:
@@ -69,7 +85,7 @@ while cap.isOpened():
                 connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_iris_connections_style()
             )
 
-    # Draw Hands landmarks
+    # Draw the hand landmarks
     if hand_results.multi_hand_landmarks:
         for hand_landmarks in hand_results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(
@@ -80,7 +96,7 @@ while cap.isOpened():
                 connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style()
             )
 
-    # Draw Pose landmarks
+    # Draw the pose landmarks
     if pose_results.pose_landmarks:
         mp_drawing.draw_landmarks(
             image=image,
@@ -101,4 +117,4 @@ cv2.destroyAllWindows()
 face_mesh.close()
 hands.close()
 pose.close()
-
+selfie_segmentation.close()
